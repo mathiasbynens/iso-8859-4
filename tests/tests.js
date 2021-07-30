c650f6618fbc6b4b128a0e6a670236a31d0affe3
Mathias Bynens <mathias@qiwi.be>
2021-07-30 12:19:34 +0200

    Rebuild library

--- tests/tests.js
+++ tests/tests.js
@@ -1,158 +1,111 @@
-(function(root) {
-	'use strict';
+const assert = require('assert');
 
-	var noop = Function.prototype;
+const iso88594 = require('../iso-8859-4.js');
 
-	var load = (typeof require == 'function' && !(root.define && define.amd)) ?
-		require :
-		(!root.document && root.java && root.load) || noop;
+console.log('Testing `iso88594.encode`…');
+assert.strictEqual(
+	iso88594.encode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
+	'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
+	'U+0000 to U+007F remain unchanged'
+);
+assert.strictEqual(
+	iso88594.encode('\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\u0104\u0138\u0156\xA4\u0128\u013B\xA7\xA8\u0160\u0112\u0122\u0166\xAD\u017D\xAF\xB0\u0105\u02DB\u0157\xB4\u0129\u013C\u02C7\xB8\u0161\u0113\u0123\u0167\u014A\u017E\u014B\u0100\xC1\xC2\xC3\xC4\xC5\xC6\u012E\u010C\xC9\u0118\xCB\u0116\xCD\xCE\u012A\u0110\u0145\u014C\u0136\xD4\xD5\xD6\xD7\xD8\u0172\xDA\xDB\xDC\u0168\u016A\xDF\u0101\xE1\xE2\xE3\xE4\xE5\xE6\u012F\u010D\xE9\u0119\xEB\u0117\xED\xEE\u012B\u0111\u0146\u014D\u0137\xF4\xF5\xF6\xF7\xF8\u0173\xFA\xFB\xFC\u0169\u016B\u02D9'),
+	'\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF',
+	'Encoding all other symbols in the character set'
+);
+assert.throws(
+	() => {
+		iso88594.encode('\uFFFF');
+	},
+	Error,
+	'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode, which is the implied default for `encode()`'
+);
+assert.throws(
+	() => {
+		iso88594.encode('\uFFFF', { mode: 'fatal' });
+	},
+	Error,
+	'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode'
+);
+assert.throws(
+	() => {
+		iso88594.encode('\uFFFF', { mode: 'FATAL' });
+	},
+	Error,
+	'Mode names are case-insensitive'
+);
+assert.throws(
+	() => {
+		iso88594.encode('\uFFFF', { mode: 'fAtAl' });
+	},
+	Error,
+	'Mode names are case-insensitive'
+);
+assert.strictEqual(
+	iso88594.encode('\uFFFF', { mode: 'html' }),
+	'&#65535;',
+	'Encoding a code point that is invalid for this encoding returns an HTML entity in `html` mode'
+);
+assert.strictEqual(
+	iso88594.encode('\uFFFF', { mode: 'HTML' }),
+	'&#65535;',
+	'Mode names are case-insensitive'
+);
+assert.strictEqual(
+	iso88594.encode('\uFFFF', { mode: 'hTmL' }),
+	'&#65535;',
+	'Mode names are case-insensitive'
+);
 
-	var QUnit = (function() {
-		return root.QUnit || (
-			root.addEventListener || (root.addEventListener = noop),
-			root.setTimeout || (root.setTimeout = noop),
-			root.QUnit = load('../node_modules/qunitjs/qunit/qunit.js') || root.QUnit,
-			addEventListener === noop && delete root.addEventListener,
-			root.QUnit
-		);
-	}());
-
-	var qe = load('../node_modules/qunit-extras/qunit-extras.js');
-	if (qe) {
-		qe.runInContext(root);
-	}
-
-	// The `iso88594` object to test
-	var iso88594 = root.iso88594 || (root.iso88594 = (
-		iso88594 = load('../iso-8859-4.js') || root.iso88594,
-		iso88594 = iso88594.iso88594 || iso88594
-	));
-
-	/*--------------------------------------------------------------------------*/
-
-	// `throws` is a reserved word in ES3; alias it to avoid errors
-	var raises = QUnit.assert['throws'];
-
-	// explicitly call `QUnit.module()` instead of `module()`
-	// in case we are in a CLI environment
-	QUnit.module('iso-8859-4');
-
-	test('iso88594.encode', function() {
-		equal(
-			iso88594.encode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
-			'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
-			'U+0000 to U+007F remain unchanged'
-		);
-		equal(
-			iso88594.encode('\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\u0104\u0138\u0156\xA4\u0128\u013B\xA7\xA8\u0160\u0112\u0122\u0166\xAD\u017D\xAF\xB0\u0105\u02DB\u0157\xB4\u0129\u013C\u02C7\xB8\u0161\u0113\u0123\u0167\u014A\u017E\u014B\u0100\xC1\xC2\xC3\xC4\xC5\xC6\u012E\u010C\xC9\u0118\xCB\u0116\xCD\xCE\u012A\u0110\u0145\u014C\u0136\xD4\xD5\xD6\xD7\xD8\u0172\xDA\xDB\xDC\u0168\u016A\xDF\u0101\xE1\xE2\xE3\xE4\xE5\xE6\u012F\u010D\xE9\u0119\xEB\u0117\xED\xEE\u012B\u0111\u0146\u014D\u0137\xF4\xF5\xF6\xF7\xF8\u0173\xFA\xFB\xFC\u0169\u016B\u02D9'),
-			'\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF',
-			'Encoding all other symbols in the character set'
-		);
-		raises(
-			function() {
-				iso88594.encode('\uFFFF');
-			},
-			Error,
-			'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode, which is the implied default for `encode()`'
-		);
-		raises(
-			function() {
-				iso88594.encode('\uFFFF', { 'mode': 'fatal' });
-			},
-			Error,
-			'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode'
-		);
-		raises(
-			function() {
-				iso88594.encode('\uFFFF', { 'mode': 'FATAL' });
-			},
-			Error,
-			'Mode names are case-insensitive'
-		);
-		raises(
-			function() {
-				iso88594.encode('\uFFFF', { 'mode': 'fAtAl' });
-			},
-			Error,
-			'Mode names are case-insensitive'
-		);
-		equal(
-			iso88594.encode('\uFFFF', { 'mode': 'html' }),
-			'&#65535;',
-			'Encoding a code point that is invalid for this encoding returns an HTML entity in `html` mode'
-		);
-		equal(
-			iso88594.encode('\uFFFF', { 'mode': 'HTML' }),
-			'&#65535;',
-			'Mode names are case-insensitive'
-		);
-		equal(
-			iso88594.encode('\uFFFF', { 'mode': 'hTmL' }),
-			'&#65535;',
-			'Mode names are case-insensitive'
-		);
-	});
-
-	test('iso88594.decode', function() {
-		equal(
-			iso88594.decode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
-			'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
-			'U+0000 to U+007F remain unchanged'
-		);
-		equal(
-			iso88594.decode('\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF'),
-			'\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\u0104\u0138\u0156\xA4\u0128\u013B\xA7\xA8\u0160\u0112\u0122\u0166\xAD\u017D\xAF\xB0\u0105\u02DB\u0157\xB4\u0129\u013C\u02C7\xB8\u0161\u0113\u0123\u0167\u014A\u017E\u014B\u0100\xC1\xC2\xC3\xC4\xC5\xC6\u012E\u010C\xC9\u0118\xCB\u0116\xCD\xCE\u012A\u0110\u0145\u014C\u0136\xD4\xD5\xD6\xD7\xD8\u0172\xDA\xDB\xDC\u0168\u016A\xDF\u0101\xE1\xE2\xE3\xE4\xE5\xE6\u012F\u010D\xE9\u0119\xEB\u0117\xED\xEE\u012B\u0111\u0146\u014D\u0137\xF4\xF5\xF6\xF7\xF8\u0173\xFA\xFB\xFC\u0169\u016B\u02D9',
-			'Decoding all other symbols in the character set'
-		);
-		equal(
-			iso88594.decode('\uFFFF'),
-			'\uFFFD',
-			'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode, which is the implied default for `decode()`'
-		);
-		equal(
-			iso88594.decode('\uFFFF', { 'mode': 'replacement' }),
-			'\uFFFD',
-			'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode'
-		);
-		equal(
-			iso88594.decode('\uFFFF', { 'mode': 'REPLACEMENT' }),
-			'\uFFFD',
-			'Mode names are case-insensitive'
-		);
-		equal(
-			iso88594.decode('\uFFFF', { 'mode': 'rEpLaCeMeNt' }),
-			'\uFFFD',
-			'Mode names are case-insensitive'
-		);
-		raises(
-			function() {
-				iso88594.decode('\uFFFF', { 'mode': 'fatal' });
-			},
-			Error,
-			'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
-		);
-		raises(
-			function() {
-				iso88594.decode('\uFFFF', { 'mode': 'FATAL' });
-			},
-			Error,
-			'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
-		);
-		raises(
-			function() {
-				iso88594.decode('\uFFFF', { 'mode': 'fAtAl' });
-			},
-			Error,
-			'Mode names are case-insensitive'
-		);
-	});
-
-	/*--------------------------------------------------------------------------*/
-
-	// configure QUnit and call `QUnit.start()` for
-	// Narwhal, Node.js, PhantomJS, Rhino, and RingoJS
-	if (!root.document || root.phantom) {
-		QUnit.config.noglobals = true;
-		QUnit.start();
-	}
-}(typeof global == 'object' && global || this));
+console.log('Testing `iso88594.decode`…');
+assert.strictEqual(
+	iso88594.decode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
+	'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
+	'U+0000 to U+007F remain unchanged'
+);
+assert.strictEqual(
+	iso88594.decode('\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF'),
+	'\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\u0104\u0138\u0156\xA4\u0128\u013B\xA7\xA8\u0160\u0112\u0122\u0166\xAD\u017D\xAF\xB0\u0105\u02DB\u0157\xB4\u0129\u013C\u02C7\xB8\u0161\u0113\u0123\u0167\u014A\u017E\u014B\u0100\xC1\xC2\xC3\xC4\xC5\xC6\u012E\u010C\xC9\u0118\xCB\u0116\xCD\xCE\u012A\u0110\u0145\u014C\u0136\xD4\xD5\xD6\xD7\xD8\u0172\xDA\xDB\xDC\u0168\u016A\xDF\u0101\xE1\xE2\xE3\xE4\xE5\xE6\u012F\u010D\xE9\u0119\xEB\u0117\xED\xEE\u012B\u0111\u0146\u014D\u0137\xF4\xF5\xF6\xF7\xF8\u0173\xFA\xFB\xFC\u0169\u016B\u02D9',
+	'Decoding all other symbols in the character set'
+);
+assert.strictEqual(
+	iso88594.decode('\uFFFF'),
+	'\uFFFD',
+	'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode, which is the implied default for `decode()`'
+);
+assert.strictEqual(
+	iso88594.decode('\uFFFF', { mode: 'replacement' }),
+	'\uFFFD',
+	'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode'
+);
+assert.strictEqual(
+	iso88594.decode('\uFFFF', { mode: 'REPLACEMENT' }),
+	'\uFFFD',
+	'Mode names are case-insensitive'
+);
+assert.strictEqual(
+	iso88594.decode('\uFFFF', { mode: 'rEpLaCeMeNt' }),
+	'\uFFFD',
+	'Mode names are case-insensitive'
+);
+assert.throws(
+	() => {
+		iso88594.decode('\uFFFF', { mode: 'fatal' });
+	},
+	Error,
+	'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
+);
+assert.throws(
+	() => {
+		iso88594.decode('\uFFFF', { mode: 'FATAL' });
+	},
+	Error,
+	'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
+);
+assert.throws(
+	() => {
+		iso88594.decode('\uFFFF', { mode: 'fAtAl' });
+	},
+	Error,
+	'Mode names are case-insensitive'
+);
